387736da5ce09f5711c3b980860e906e9fe85402
sivakumarreddym <info@joindevops.com>
2024-08-26 13:02:56 +0530

    functions

---
+++ install-packages.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date "+%Y-%m-%d-%H-%M-%S")
+echo $0
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "Please run this script with root priveleges"
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 is... $G SUCCESS $N"
+    fi
+}
+
+CHECK_ROOT
+
+for package in $@
+do
+
+done
