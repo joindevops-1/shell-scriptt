8e1eb9c7c2b216e85c6593b782de3a8382f56880
sivakumarreddym <info@joindevops.com>
2024-08-26 13:19:46 +0530

    functions

--- install-packages.sh
+++ install-packages.sh
@@ -3,6 +3,7 @@
 USERID=$(id -u)
 TIMESTAMP=$(date "+%Y-%m-%d-%H-%M-%S")
 SCRIPTNAME=$(echo $0 | cut -d "." -f1)
+mkdir -p /var/log/shellscript-logs
 LOGFILE="/var/log/shellscript-logs/$SCRIPTNAME-$TIMESTAMP.log"
 R="\e[31m"
 G="\e[32m"
